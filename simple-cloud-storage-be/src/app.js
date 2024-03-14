3ed0610344e92d87ce9dd6bed325fa114f619d18
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-03-14 14:41:29 +0530

    Server is created and dotenv configuraed

---
+++ simple-cloud-storage-be/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = express.Router();
+const bodyParser = require('body-parser');
+const { PORT_NUMBER } = require('./config/env.config');
+
+// Initializing our application with express
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res, next) => {
+  res.status(200).send('Welcome to Simple Could Storage');
+});
+
+// Creating and Running a server
+const PORT = PORT_NUMBER || 8080;
+app.listen(PORT, (err) => {
+  if (!err) {
+    console.log(`Server listening on PORT ${PORT}`);
+  } else {
+    console.log(`Some error encountered to run the server ${err}`);
+  }
+});
---
+++ simple-cloud-storage-be/src/config/env.config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const { PORT_NUMBER } = process.env;
+
+module.exports = {
+  PORT_NUMBER
+};
