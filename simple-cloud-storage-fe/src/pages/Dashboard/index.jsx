1bc6714cd2d1ddaa6ab6d6aa1d304027b02b8277
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-04-01 21:58:09 +0530

    user details mapping done

--- simple-cloud-storage-fe/src/pages/Dashboard/index.jsx
+++ simple-cloud-storage-fe/src/pages/Dashboard/index.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import Button from "../../components/Button";
+import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
+  const { userSCSData } = useSelector((state) => state.simpleCloudStorage);
+  const [userName] = useState(userSCSData?.name);
+  const [email] = useState(userSCSData?.email);
+  const [userId] = useState(userSCSData?.userId);
+
   return (
     <div className="dashboard__container">
       <div className="mainDashboardContainer">
-        <h2>Hello! Test User 🚀</h2>
+        <div className="userInfo">
+          <h2>Hello! {userName} 🚀</h2>
+          <h3>Email: {email}</h3>
+          <h3>User Id: {userId}</h3>
+        </div>
         <section className="createBucket">
           <h4>Welcome! Handle your file storage using Simple Cloud Storage</h4>
           <Button btnType={'button'}>Create Bucket</Button>
